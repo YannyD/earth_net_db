65fd270c9f81801f3ecdfbcd5a9d452220816c90
YannyD <yan@wemeta.world>
2023-07-26 21:04:38 -0500

    Initial security tests

--- tests/security.test.ts
+++ tests/security.test.ts
@@ -20,6 +20,7 @@ import assert from "assert";
 import { describe, beforeEach, before } from "mocha";
 import { ref, set, get } from "firebase/database";
 import { readFileSync, createWriteStream } from "node:fs";
+import { async } from "@firebase/util";
 
 describe("earth security rules tests", () => {
   let testEnv: RulesTestEnvironment;
@@ -36,12 +37,74 @@ describe("earth security rules tests", () => {
     });
   });
 
-  it("should not allow read access to all users", async () => {
+  it("should not allow read access to users data for unauthenticated user", async () => {
     const unAuthDb = testEnv.unauthenticatedContext().database();
     await assertFails(get(ref(unAuthDb, "/users")));
   });
-});
 
-/*
-Creating and configuring a RulesTestEnvironment with a call to initializeTestEnvironment.
-*/
+  it("should  allow read access to user data for authenticated user", async () => {
+    const authDb = testEnv
+      .authenticatedContext("0rrYgiTiCPVPK6jrW16eZE8MDvl2")
+      .database();
+    await assertSucceeds(
+      get(ref(authDb, "/users/0rrYgiTiCPVPK6jrW16eZE8MDvl2"))
+    );
+  });
+
+  //Todo add tests for write access to users data
+
+  it("should  allow read access to permission data for authenticated user", async () => {
+    const authDb = testEnv
+      .authenticatedContext("0rrYgiTiCPVPK6jrW16eZE8MDvl2")
+      .database("http://127.0.0.1:4001/?ns=homeearthnet");
+    const result = (
+      await get(ref(authDb, "/permissions/0rrYgiTiCPVPK6jrW16eZE8MDvl2"))
+    ).val();
+    console.log("result:", result);
+    await assertSucceeds(
+      get(ref(authDb, "/permissions/0rrYgiTiCPVPK6jrW16eZE8MDvl2"))
+    );
+  });
+  it("should  not allow read access to permission data for unauthenticated user", async () => {
+    const unAuthDb = testEnv
+      .unauthenticatedContext()
+      .database("http://127.0.0.1:4001/?ns=homeearthnet");
+    await assertFails(
+      get(ref(unAuthDb, "/permissions/0rrYgiTiCPVPK6jrW16eZE8MDvl2"))
+    );
+  });
+  it("should  not allow read access to permission data for different authenticated user", async () => {
+    const authDb = testEnv
+      .authenticatedContext("0rrYgiTiCPVPK6jrW16eZE8MDvl")
+      .database("http://127.0.0.1:4001/?ns=homeearthnet");
+    await assertFails(
+      get(ref(authDb, "/permissions/0rrYgiTiCPVPK6jrW16eZE8MDvl2"))
+    );
+  });
+
+  it("should not allow write access to permission data for unauthenticated user", async () => {
+    const unAuthDb = testEnv
+      .unauthenticatedContext()
+      .database("http://127.0.0.1:4001/?ns=homeearthnet");
+    await assertFails(
+      set(ref(unAuthDb, "/permissions/0rrYgiTiCPVPK6jrW16eZE8MDvl2"), true)
+    );
+  });
+  it("should not allow write access to permission data for authenticated user", async () => {
+    const authDb = testEnv
+      .authenticatedContext("0rrYgiTiCPVPK6jrW16eZE8MDvl2")
+      .database("http://127.0.0.1:4001/?ns=homeearthnet");
+    await assertFails(
+      set(ref(authDb, "/permissions/0rrYgiTiCPVPK6jrW16eZE8MDvl2"), true)
+    );
+  });
+
+  it("should allow read access to property only for permitted users", async () => {
+    const authDb = testEnv
+      .authenticatedContext("0rrYgiTiCPVPK6jrW16eZE8MDvl2")
+      .database("http://127.0.0.1:4001/?ns=homeearthnet");
+    const value = (await get(ref(authDb, "/properties/"))).val();
+    console.log("value:", value);
+    await assertSucceeds(get(ref(authDb, "/properties")));
+  });
+});
